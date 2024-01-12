beb887e8d08c5ac5d70fc8f34c0c2243c6dd7bb9
shiladitya <155450322+shiladityasarkar@users.noreply.github.com>
2024-01-13 01:00:53 +0530

    version 0.5.2
    corrected the issue with report_.

--- build/lib/mysticML/Preprocess.py
+++ build/lib/mysticML/Preprocess.py
@@ -3,10 +3,9 @@ from mysticML.utils import Duplicate, Outlier, \
     Imputation, Transformation, Encoding, \
     FeatureSelection, DimReduction, Sampling
 
-
 class Preprocess(object):
     def __init__(self, df:pandas.DataFrame) ->None:
-        self.report_ = None
+        self.report_ = []
         self.df = df
 
     def fit(self, target:str=None, combo:str='advanced',
--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 setup(name='mysticML',
-      version='0.5.1')
+      version='0.5.2')
